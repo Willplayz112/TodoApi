e7ca90af0ac7a038e95c54cec7e1830a2f2ae5d3
Willplayz112 <110480301+Willplayz112@users.noreply.github.com>
2025-09-22 08:55:55 +0100

    Fix: Removed unused libs

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-//using Microsoft.IdentityModel.Tokens;
-//using System.IdentityModel.Tokens.Jwt;
-//using System.Security.Claims;
-//using System.Text;
 using TodoApi.Models;
 
 
